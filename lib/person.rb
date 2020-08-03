333ce0b55fdb032254ae7b6e0e14ab8425cde1e4
wlcreate <waverley.nyc@gmail.com>
2020-08-03 16:42:34 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def name
+        @name
+    end
+
+    def name=(name_arg)
+        @name = name_arg
+    end
+
+    def job
+        @job
+    end
+
+    def job=(job_arg)
+        @job = job_arg
+    end
+end
