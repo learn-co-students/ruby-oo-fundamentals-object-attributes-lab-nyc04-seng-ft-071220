4166650930f9cdb914b2943df52362e82cfd1220
ChristyTropila <tropila04@gmail.com>
2020-08-03 16:22:04 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+
+
+
+    def name=(name_arg)
+        @name=name_arg
+
+    end
+
+    def name
+       @name
+
+    end
+
+    def breed=(breed_arg)
+        @breed=breed_arg
+
+    end
+
+    def breed
+        @breed
+
+    end
+
+end
