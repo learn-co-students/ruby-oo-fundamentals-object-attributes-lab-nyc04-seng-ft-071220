dffe0156ccb15b9061d6feea6577da360c9d9794
hortencia c <hortenciaecisneros@gmail.com>
2020-08-05 13:06:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog 
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name=name
+  end
+  
+  def breed 
+    @breed
+  end
+  
+  def breed=(breed)
+    @breed=breed
+  end
+  
+end
+
+# define the name of the dog "name" -->@name
+# create the name of the dog and set it to instance variable def name=(name) ---> @name=name
+# do the same for breed !
---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person 
+  
+  def name 
+    @name 
+  end
+  
+  def name=(name)
+    @name=name
+  end
+  
+   
+  def job
+    @job
+  end
+  
+  
+  def job=(job)
+    @job=job
+  end
+ 
+  
+end
+
+
+# define a person -- create an instance variable @name 
+#  reds the name of the person from the instance variable from @name should look like def name=(name) @name=name.
+# do the same for job !
